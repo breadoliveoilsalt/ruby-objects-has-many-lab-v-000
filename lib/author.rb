86745714509115810d72c215c3c4aace71cb2c3e
Tony Distinti <adistinti@gmail.com>
2017-09-28 19:53:15 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,9 +1,21 @@
 class Author
 
-  attr_accessor :posts,
+  attr_accessor :posts, :name, :author
 
   def initialize(name)
     @posts = []
+    @name = name
   end
+
+  def add_post(post)
+    @posts << post
+    post.author = self 
+  end
+
+    def add_post_by_title(title)
+      p = Post.new(title)
+      p.author = self
+      @posts << p
+    end
 
 end
